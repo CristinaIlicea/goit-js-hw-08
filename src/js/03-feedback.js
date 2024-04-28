33e544e31ae9bafe912b73808d2a3859ab5dfc9c
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-04-28 21:04:33 +0300

    ext 3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,14 @@
 
-  const input = document.querySelector(".feedback-form");
+  const form = document.querySelector(".feedback-form");
   const button = document.querySelector(".button");
   const saved_value_key = "feedback-form-state";
   
   const handleClick = () => {
-    e.preventDefault();
-    localStorage.setItem(saved_value_key, input.value);
-    localStorage.clear();
-    console.log("email", "message");
+    handleClick.preventDefault();
+    localStorage.setItem(saved_value_key, form.value);
+    localStorage.removeItem(saved_value_key);
+    form.reset();
+    console.log(saved_value_key);
   };
 
   button.addEventListener("click", handleClick);
