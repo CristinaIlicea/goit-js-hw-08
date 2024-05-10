736dc078123c8a772a447458ef749b9f812a365b
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-05-10 16:27:01 +0300

    ex 1 simplelight box gallery fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,23 @@
-// Descris în documentație
-import SimpleLightbox from "simplelightbox";
-// Import suplimentar de stil
-import '_../../node_modules/simplelightbox/dist_'
-
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector('.gallery');
+galleryItems.forEach(item => {
+  const listItemHTML = `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`;
+  gallery.insertAdjacentHTML('beforeend', listItemHTML);
+});
+let lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
